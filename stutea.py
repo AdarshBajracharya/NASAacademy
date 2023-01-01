4324c7529404b58c069dc0f0175212291421db41
unknown <abajracharya55@gmail.com>
2023-01-01 17:29:02 +0545

    frameworks

--- stutea.py
+++ stutea.py
@@ -1,16 +1,19 @@
 from tkinter import *
+from loginteach import *
+from loginstu import *
 from PIL import Image, ImageTk
 
-win = Tk()
-win.title("NASA Academy")
-my_image = ImageTk.PhotoImage(Image.open("logo.png"))
-my_label = Label(image=my_image)
-my_label.pack()
-im = Label(win, text = " I am a..", font = 15)
-im.pack()
-teach = Button(win, text = "Teacher")
-teach.pack()
-stu = Button(win, text = "Student")
-stu.pack()
+def stutea():
+    win = Tk()
+    win.title("NASA Academy")
+    my_image = ImageTk.PhotoImage(Image.open("logo.png"))
+    my_label = Label(image=my_image)
+    my_label.pack()
+    im = Label(win, text = " I am a..", font = 15)
+    im.pack()
+    teach = Button(win, text = "Teacher")
+    teach.pack()
+    stu = Button(win, text = "Student")
+    stu.pack()
 
-win.mainloop()
+    win.mainloop()
