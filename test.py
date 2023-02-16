acf530cd9e7c6adf5a29f33ba3692fb4dca44084
Aashrit17 <aashrit17038gmail.comgit config --global user.name Aashrit17>
2023-02-16 13:42:36 +0545

    c 1

--- test.py
+++ test.py
@@ -14,3 +14,13 @@ frame3=Frame(frame2,bg="red",width=90,height=600)
 frame3.pack(padx=1,pady=1,side=LEFT)
 frame3.propagate(False)
 l8=Label(frame3,text="6AM",fg="black").pack(padx=30,pady=30)
+
+
+frame4=Frame(frame2,bg="blue",width=150,height=600)
+frame4.pack(padx=1,pady=1,side=LEFT)
+frame4.propagate(False)
+b= Button(frame4, text="English",fg="black")
+b.place(x=50,y=30)
+b= Button(frame4, text="Computer",fg="black")
+b.place(x=50,y=90)
+l7=Label(frame4,text="Sunday",fg="black").pack(padx=1,pady=1)
