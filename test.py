0e69b5152fc1dbf3f0629fbc0eeebbbe7c5706fb
Aashrit17 <aashrit17038gmail.comgit config --global user.name Aashrit17>
2023-02-16 13:38:33 +0545

    commit

--- test.py
+++ test.py
@@ -8,3 +8,9 @@ frame1.propagate(False)
 frame2 = Frame(frame1,bg="white",width=1500,height=1500)
 frame2.pack(pady=10,padx=10)
 frame2.propagate(False)
+
+
+frame3=Frame(frame2,bg="red",width=90,height=600)
+frame3.pack(padx=1,pady=1,side=LEFT)
+frame3.propagate(False)
+l8=Label(frame3,text="6AM",fg="black").pack(padx=30,pady=30)
