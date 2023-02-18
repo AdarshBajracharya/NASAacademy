d67201f24bb49db32836a77c7affa333b839868a
Suyogya17 <suyogyashrestha17@gmail.com>
2023-02-18 09:27:12 +0545

    edited

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ def main():
     teac = Image.open("images\\teacher.png")     
     timg= teac.resize((300, 300))  
     reteach = ImageTk.PhotoImage(timg)     
-
+	
     my_image = Image.open("images\\logoo.png")     
     img= my_image.resize((300, 300))  
     reimg = ImageTk.PhotoImage(img)      
@@ -333,7 +333,375 @@ def routin():
     l16=Label(frame3,text="2PM",fg="black").pack(padx=10,pady=10)
     l17=Label(frame3,text="3PM",fg="black").pack(padx=10,pady=10)
 
+def homepage():
+
+    def clickrout():
+        frame.pack_forget()
+        routin()
+
+    def clickstu():
+        frame.pack_forget()
+        database()
+        
+
+    frame = Frame(win,width=800,height=800, bd = 10, highlightthickness=3, highlightbackground="black")
+    frame.pack(padx=20, pady=20)
+    frame.pack_propagate(False)
+
+   
+
+    my_label = Label(frame, image=reimg, borderwidth=0)
+    my_label.place(x = 250, y = 70)
+
     
+    routine = Button(frame, text = "Routine", width = 15, height=1, font =("Heavitas", 20), activebackground= "grey",bg="white",command = clickrout)
+    routine.place(x = 230, y = 420)
+
+    students = Button(frame, text = "Students", width = 15, height=1, font =("Heavitas", 20), activebackground= "grey",bg="white",command = clickstu)
+    students.place(x = 230, y = 490)
+
+    result = Button(frame, text = "NOTICE", width = 15, height=1, font =("Heavitas", 20), activebackground= "grey",bg="white")
+    result.place(x = 230, y = 560)
+
+    acc_details = Button(frame, text = "Account", width = 15, height=1, font =("Heavitas", 20), activebackground= "grey",bg="white")
+    acc_details.place(x = 230, y = 630)
+    
+def database():
+
+    def clickback():
+        frame.pack_forget()
+        homepage()
+
+    
+    frame = Frame(win,width=1350,height=800, bd = 10, highlightthickness=3, highlightbackground="black")
+    frame.pack(padx=20, pady=20)
+    frame.pack_propagate(False)
+
+    button = Button(frame, text = "Go back", font = ("heavitas",15), bg = "blue", fg = "white",command = clickback)
+    button.place(x =0, y = 0)
+
+
+        
+    Frame1=Frame(frame,bd=4,relief=RIDGE,bg="grey")
+    Frame1.place(x=20,y=100,width=450,height=590)
+
+    title1= Label(Frame1,text="Manage Students",bd=10,relief=GROOVE,fg="white",font=("Heavitas",20,"bold"),bg="red")
+    title1.grid(row=0,columnspan=2,pady=20)
+
+    # create a text box and label ---- 
+
+    label_name= Label(Frame1,text="Student Name: ",bg="grey",fg="white",font=("times new roman",15,"bold"))
+    label_name.grid(row=1,column=0,pady=10,padx=20,sticky="w")
+
+    text_name= Entry(Frame1,font=("times new roman",15,"bold"),bd=5,relief=GROOVE)
+    text_name.grid(row=1,column=1,pady=10,padx=20,sticky="w")
+
+    label_roll= Label(Frame1,text="Roll No: ",bg="grey",fg="white",font=("times new roman",15,"bold"))
+    label_roll.grid(row=2,column=0,pady=10,padx=20,sticky="w")
+
+    text_roll= Entry(Frame1,font=("times new roman",15,"bold"),bd=5,relief=GROOVE)
+    text_roll.grid(row=2,column=1,pady=10,padx=20,sticky="w")
+
+    label_gender= Label(Frame1,text="Gender: ",bg="grey",fg="white",font=("times new roman",15,"bold"))
+    label_gender.grid(row=3,column=0,pady=10,padx=20,sticky="w")
+
+    text_gender= Entry(Frame1,font=("times new roman",15,"bold"),bd=5,relief=GROOVE)
+    text_gender.grid(row=3,column=1,pady=10,padx=20,sticky="w")
+
+    combo_gender= ttk.Combobox(Frame1,font=("times new roman",13,"bold"),state="readonly")
+    combo_gender['values']=("Male","Female")
+    combo_gender.grid(row=3,column=1,padx=20,pady=10)
+
+    label_section= Label(Frame1,text="Section: ",bg="grey",fg="white",font=("times new roman",15,"bold"))
+    label_section.grid(row=4,column=0,pady=10,padx=20,sticky="w")
+
+    text_section= Entry(Frame1,font=("times new roman",15,"bold"),bd=5,relief=GROOVE)
+    text_section.grid(row=4,column=1,pady=10,padx=20,sticky="w")
+
+    label_subject= Label(Frame1,text="Subject: ",bg="grey",fg="white",font=("times new roman",15,"bold"))
+    label_subject.grid(row=5,column=0,pady=10,padx=20,sticky="w")
+
+    text_subject= Entry(Frame1,font=("times new roman",15,"bold"),bd=5,relief=GROOVE)
+    text_subject.grid(row=5,column=1,pady=10,padx=20,sticky="w")
+
+    combo_total_marks= ttk.Combobox(Frame1,font=("times new roman",13,"bold"),state="readonly")
+    combo_total_marks['values']=("Science","Maths","English","Nepali","Social","Computer")
+    combo_total_marks.grid(row=5,column=1,padx=20,pady=10)
+
+
+    label_obt_marks= Label(Frame1,text="Obtain Marks: ",bg="grey",fg="white",font=("times new roman",15,"bold"))
+    label_obt_marks.grid(row=6,column=0,pady=10,padx=20,sticky="w")
+
+    text_obt_marks= Entry(Frame1,font=("times new roman",15,"bold"),bd=5,relief=GROOVE)
+    text_obt_marks.grid(row=6,column=1,pady=10,padx=20,sticky="w")
+
+    label_total_marks= Label(Frame1,text="Total Marks: ",bg="grey",fg="white",font=("times new roman",15,"bold"))
+    label_total_marks.grid(row=7,column=0,pady=10,padx=20,sticky="w")
+
+    text_total_marks= Entry(Frame1,font=("times new roman",15,"bold"),bd=5,relief=GROOVE)
+    text_total_marks.grid(row=7,column=1,pady=10,padx=20,sticky="w")
+
+    combo_total_marks= ttk.Combobox(Frame1,font=("times new roman",13,"bold"),state="readonly")
+    combo_total_marks['values']=("75","100")
+    combo_total_marks.grid(row=7,column=1,padx=20,pady=10)
+
+    #==== Frame 2 ================
+
+    Frame2=Frame(frame,bd=4,relief=RIDGE,bg="grey")
+    Frame2.place(x=500,y=100,width=800,height=590)
+
+    lbl_search=Label(Frame2,text="Search By: ",bg="grey",fg="white",font=("times new roman",20,"bold"))
+    lbl_search.grid(row=0,column=0,padx=20,pady=20,sticky="w")
+
+    combo_search= ttk.Combobox(Frame2,width=15,font=("times new roman",13,"bold"),state="readonly")
+    combo_search['values']=("Roll No","StudentName")
+    combo_search.grid(row=0,column=1,padx=20,pady=10)
+
+    text_Search= Entry(Frame2,width=15,font=("times new roman",15,"bold"),bd=5)
+    text_Search.grid(row=0,column=2,pady=10,padx=20,sticky="w")
+
+
+
+
+
+
+
+    def config() -> None:
+        # Creating a data table
+
+        conn=sqlite3.connect('Students.db')
+        c=conn.cursor()
+
+        c.execute(""" CREATE TABLE student(
+            StudentName text,
+            RollNo integer PRIMARY KEY,
+            Gender text,
+            Subject text,
+            Section text,
+            ObtMarks integer,
+            FinalMarks integer
+            )""")
+        print("database created successfully")
+        conn.commit()
+        conn.close()
+
+    def initialize():
+        global root,text_name,text_roll,text_gender,text_subject,text_section,text_obt_marks,text_total_marks,Updatebttm,Deletebttm
+
+    #  DELETE function ------------
+
+    def DELETE():
+        conn= sqlite3.connect('Students.db')
+
+        c= conn.cursor()
+
+        c.execute("DELETE from student WHERE oid =  " + text_Search.get())
+        print('Delete Sucessfully')
+
+        c.execute("SELECT *, oid FROM student")
+
+        records= c.fetchall()
+        print(records)
+
+        print_record=''
+        for record in records:
+            print_record += str(record[0])+' '+str(record[1])+' '+'\t'+str(record[4])+"\n"
+
+        query_label= Label(root,text= print_record)
+        query_label.grid(row=8, column=0, columnspan=2)
+
+        messagebox.showinfo("Alert","Deleted Sucessfully")
+
+        conn.commit()
+        conn.close()
+
+    # ADD function --------------
+        
+    def ADD():
+        conn=sqlite3.connect('Students.db')
+        c=conn.cursor()
+        
+        c.execute('INSERT INTO student VALUES (:StudentName, :RollNo, :Gender, :Subject, :Section, :ObtMarks, :FinalMarks)',
+            {
+            'StudentName': text_name.get(),
+            'RollNo': text_roll.get(),
+            'Gender': text_gender.get(),
+            'Subject': text_subject.get(),
+            'Section': text_section.get(),
+            'ObtMarks': text_obt_marks.get(),
+            'FinalMarks': text_total_marks.get()
+            
+            })
+        # print('data submitted successfully')
+
+        text_name.delete(0,END)
+        text_roll.delete(0,END)
+        text_gender.delete(0,END)
+        text_subject.delete(0,END)
+        text_section.delete(0,END)
+        text_obt_marks.delete(0,END)
+        text_total_marks.delete(0,END)
+        
+        conn.commit()
+
+        conn.close()
+
+    # SHOW query ------------
+
+    def QUERY():
+
+        conn= sqlite3.connect('Students.db')
+
+        c= conn.cursor()
+
+        c.execute("SELECT *, oid FROM student")
+
+        records= c.fetchall()
+        print(records)
+
+        print_record=' '
+        for record in records:
+            print_record += str(record[0])+ " " +str(record[1])+ " " + "\t" +str(record[6])+"\n"
+
+        query_label= Label(student_table,text= print_record)
+        query_label.grid(row=0,column=4,padx=10,pady=10)
+
+        conn.commit()
+        conn.close()
+
+    def edit():
+
+        global root,text_name_edit,text_roll_edit,text_gender_edit,text_subject_edit,text_section_edit,text_obtmarks_edit,text_finalmarks_edit
+        
+
+        root= Toplevel(root)
+        root.title('Update Data')
+        root.geometry('800x800')
+
+        conn= sqlite3.connect('Students.db')
+
+        c=conn.cursor()
+
+        record_id= UPDATE.get()
+
+        c.execute('SELECT * FROM student WHERE oid='+ record_id)
+
+        records = c.fetchall()
+
+        # print(records)
+        
+        Label(root,text="StudentName").grid(row=0,column=0,padx=(50,0))
+        text_name_edit=Entry(root)
+        text_name_edit.grid(row=0,column=1,pady=10)
+
+        Label(root,text="RollNo").grid(row=1,column=0)
+        text_roll_edit=Entry(root)
+        text_roll_edit.grid(row=1,column=1)
+
+        Label(root,text="Gender").grid(row=2,column=0)
+        text_gender_edit=Entry(root)
+        text_gender_edit.grid(row=2,column=1,pady=10)
+
+        Label(root,text="Subject").grid(row=3,column=0)
+        text_subject_edit=Entry(root)
+        text_subject_edit.grid(row=3,column=1)
+
+        Label(root,text="Section").grid(row=4,column=0)
+        text_section_edit=Entry(root)
+        text_section_edit.grid(row=4,column=1,pady=10)
+
+        Label(root,text="Obtained Marks").grid(row=5,column=0)
+        text_obtmarks_edit=Entry(root)
+        text_obtmarks_edit.grid(row=5,column=1)
+
+        Label(root,text="Final Marks").grid(row=6,column=0)
+        text_finalmarks_edit=Entry(root)
+        text_finalmarks_edit.grid(row=6,column=1,pady=10)
+
+        for record in records:
+            text_name_edit.insert(0,record[0])
+            text_roll_edit.insert(0,record[1])
+            text_gender_edit.insert(0,record[2])
+            text_subject_edit.insert(0,record[3])
+            text_section_edit.insert(0,record[4])
+            text_obtmarks_edit.insert(0,record[5])
+            text_finalmarks_edit.insert(0,record[6])
+            
+        conn.commit()
+        conn.close()
+
+    def UPDATE():
+        conn= sqlite3.connect('Students.db')
+
+        c= conn.cursor()
+
+        c.execute("""UPDATE student SET
+            StudentName= :StudentName,
+            RollNo= :RollNo,
+            Gender= :Gender,
+            Subject= :Subject,
+            Section= :Section,
+            ObtainMarks= :ObtainMarks,
+            FinalMarks= :FinalMarks,
+            WHERE oid= :oid""",{
+            'uname':text_name_edit.get(),
+            'fname':text_roll_edit.get(),
+            'lname':text_gender_edit.get(),
+            'age':text_subject_edit.get(),
+            'tpercent':text_section_edit.get(),
+            'address':text_obtmarks_edit.get(),
+            'phonenumber':text_finalmarks_edit.get(),
+            'oid':UPDATE.get()
+        })
+        conn.commit()
+        conn.close()
+        root.destroy()
+
+
+    # Button frame ---------------
+
+    bttm_Frame1=Frame(Frame1,bd=4,bg="grey")
+    bttm_Frame1.place(x=15,y=500,width=420)
+
+    Addbttm= Button(bttm_Frame1,text="ADD",font = ("Didot",10),width=10,command=ADD).grid(row=0,column=0,padx=10,pady=10)
+    Updatebttm= Button(bttm_Frame1,text="UPDATE",font = ("Didot",10),width=10,command=UPDATE).grid(row=0,column=1,padx=10,pady=10)
+    querybttm= Button(bttm_Frame1,text="SHOW",font = ("Didot",10),width=10,command=QUERY).grid(row=0,column=3,padx=10,pady=10)
+    Searchbttm= Button(Frame2,text="SEARCH",font = ("Didot",10),width=10,command=QUERY).grid(row=0,column=3,padx=10,pady=10)
+    Deletebttm= Button(Frame2,text="DELETE",font = ("Didot",10),width=10,command=DELETE).grid(row=0,column=4,padx=10,pady=10)
+
+    # Table frame ---------------
+
+    table_frame= Frame(Frame2,bd=4, relief=RIDGE,bg="white")
+    table_frame.place(x=10,y=70,width=770,height=500)
+
+    # scroll -------------------
+
+    scroll_x= Scrollbar(table_frame,orient=HORIZONTAL)
+    scroll_y= Scrollbar(table_frame,orient=VERTICAL)
+
+    # student_table-----------   
+
+    student_table= ttk.Treeview(table_frame,columns=("StudentName","RollNo","Gender","Subject","Section","ObtainMarks","FinalMarks"),xscrollcommand=scroll_x.set,yscrollcommand=scroll_y.set)
+    scroll_x.pack(side=BOTTOM,fill=X)
+    scroll_y.pack(side=RIGHT,fill=Y)
+    scroll_x.config(command=student_table.xview)
+    scroll_y.config(command=student_table.yview)
+    student_table.heading("StudentName",text="StudentName")
+    student_table.heading("RollNo",text="RollNo")
+    student_table.heading("Gender",text="Gender")
+    student_table.heading("Subject",text="Subject")
+    student_table.heading("Section",text="Section")
+    student_table.heading("ObtainMarks",text="ObtainMarks")
+    student_table.heading("FinalMarks",text="FinalMarks")
+    student_table['show']='headings'
+    student_table.pack(fill=BOTH,expand=1)
+
+
+    try:# Creates tables for new host machine.
+        config()
+    except sqlite3.OperationalError:
+        pass
+
 
 if __name__ == "__main__":
     main()
